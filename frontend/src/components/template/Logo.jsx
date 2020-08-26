c064d9b42ce612988955e7933778752db96054a8
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-08-26 11:49:36 -0300

    style: Logo

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import logo from '../../assets/img/logo.png'
+
 import './Logo.css'
 
 // Componente funcional
-export default props => 
+export default props =>
   <aside className="logo">
-    Logo
+    <a href="/" className="logo">
+      <img src={logo} alt="logo" />
+    </a>
   </aside>
