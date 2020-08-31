634bdbee4b768449a74e0ee7d909f0b29f7bd446
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-08-31 17:47:16 -0300

    feat: React-router and react-router-dom

--- frontend/src/components/template/Logo.jsx
+++ frontend/src/components/template/Logo.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom'
 import logo from '../../assets/img/logo.png'
 
 import './Logo.css'
@@ -7,7 +8,7 @@ import './Logo.css'
 // Componente funcional
 export default props =>
   <aside className="logo">
-    <a href="/" className="logo">
+    <Link to="/" className="logo">
       <img src={logo} alt="logo" />
-    </a>
+    </Link>
   </aside>
--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+
 
 import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
-import Home from '../components/home/Home'
 import Footer from '../components/template/Footer'
+import Routes from './Routes'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
@@ -11,9 +13,11 @@ import './App.css'
 
 
 export default props =>
-  <div className="app">
-    <Logo />
-    <Nav />
-    <Home />
-    <Footer />
-  </div>
+  <BrowserRouter>
+    <div className="app">
+      <Logo />
+      <Nav />
+      <Routes />
+      <Footer />
+    </div>
+  </BrowserRouter>
